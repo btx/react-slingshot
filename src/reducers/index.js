b1b4359df06c608ca1f2101e2a70a56eb7ecdfe8
Zdenek Softic <zdenek@softic.cz>
2016-05-03 11:17:37 +0200

    Added react-redux-router

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import fuelSavingsAppState from './fuelSavings';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
-  fuelSavingsAppState
+  fuelSavingsAppState,
+  routing: routerReducer
 });
 
 export default rootReducer;
